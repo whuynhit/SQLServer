8fe3a5202d7824ae2f01d7e27ea937916c325173
whuynhit <142070438+whuynhit@users.noreply.github.com>
2025-09-19 08:45:24 -0700

    Update T-SQL Server Active Connections.sql

--- T-SQL/T-SQL Server Active Connections.sql
+++ T-SQL/T-SQL Server Active Connections.sql
@@ -3,10 +3,13 @@ SELECT
 	c.session_id,
 	c.client_net_address,
 	s.login_name,
+	d.name AS [database],
 	c.connect_time,
 	c.last_read,
 	c.last_write
 FROM
 	sys.dm_exec_connections c
 JOIN sys.dm_exec_sessions s
-ON c.session_id = s.session_id;
+ON c.session_id = s.session_id
+JOIN sys.databases d
+ON d.database_id = s.database_id;
