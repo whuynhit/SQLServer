f7a9bac20dc982b0f880b2108879ee7d32312611
whuynhit <142070438+whuynhit@users.noreply.github.com>
2025-11-18 08:15:33 -0800

    Update T-SQL Server Active Connections.sql

--- T-SQL/T-SQL Server Active Connections.sql
+++ T-SQL/T-SQL Server Active Connections.sql
@@ -1,5 +1,12 @@
 -- Queries Server Active Connections
+DECLARE @ServerName NVARCHAR(128) = UPPER(CAST(SERVERPROPERTY('MachineName') AS nvarchar(128)));
+DECLARE @InstanceName NVARCHAR(128) = UPPER(CAST(SERVERPROPERTY('ServerName') AS nvarchar(128)));
 SELECT
+	@ServerName AS [Server_Name],
+	@InstanceName AS [Instance_Name],
+	(SELECT dec.local_net_address 
+	FROM sys.dm_exec_connections AS dec 
+	WHERE dec.session_id = @@SPID) AS [IP_Address],
 	c.session_id,
 	c.client_net_address,
 	s.login_name,
