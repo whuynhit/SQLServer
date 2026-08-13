b46648a7ac1d82fc95c20451db208c36bf8c3f8b
whuynhit <142070438+whuynhit@users.noreply.github.com>
2026-08-13 13:08:29 -0700

    Update RDS SQL Server Native Backup Restore.sql

--- T-SQL/RDS SQL Server Native Backup Restore.sql
+++ T-SQL/RDS SQL Server Native Backup Restore.sql
@@ -1,10 +1,11 @@
 -- Generate RESTORE (from bak file in S3) command based on existing databases from Source. 
+DECLARE @s3_bucket_arn NVARCHAR(256) = 'arn:aws:s3:::<bucket_name>';
 SELECT 
 	CONCAT_WS(
 	CHAR(13) + CHAR(10),
 	'exec msdb.dbo.rds_restore_database',
 	CONCAT('@restore_db_name= ''', name, ''','),
-	CONCAT('@s3_arn_to_restore_from=''arn:aws:s3:::<bucket_name>/', name, '_copy_only.bak'';')
+	CONCAT('@s3_arn_to_restore_from=''', @s3_bucket_arn,'/', name, '_copy_only.bak'';')
 	)
 FROM sys.databases
 WHERE database_id > 4
