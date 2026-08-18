2ae6b6f7952d5ff39c79d5170273598fb6bff274
whuynhit <142070438+whuynhit@users.noreply.github.com>
2026-08-18 15:45:32 -0700

    Update RDS SQL Server Native Backup Restore.sql

--- T-SQL/RDS SQL Server Native Backup Restore.sql
+++ T-SQL/RDS SQL Server Native Backup Restore.sql
@@ -1,11 +1,11 @@
 -- Generate BACKUP (bak file to S3) command based on existing databases from Source RDS. 
-DECLARE @s3_bucket_arn NVARCHAR(256) = 'arn:aws:s3:::<bucket_name>';
+DECLARE @s3_bucket_name NVARCHAR(256) = '<bucket_name>';
 SELECT 
 	CONCAT_WS(
 	CHAR(13) + CHAR(10),
 	'exec msdb.dbo.rds_backup_database',
 	CONCAT('@source_db_name= ''', name, ''','),
-	CONCAT('@s3_arn_to_backup_to=''', @s3_bucket_arn,'/', name, '_db_backup.bak'';')
+	CONCAT('@s3_arn_to_backup_to=''arn:aws:s3:::', @s3_bucket_name,'/', name, '_db_backup.bak'';')
 	)
 FROM sys.databases
 WHERE database_id > 4 AND name NOT IN ('rdsadmin')
@@ -13,13 +13,13 @@ ORDER BY name;
 
 
 -- Generate RESTORE (from bak file in S3) command based on existing databases from Source. 
-DECLARE @s3_bucket_arn NVARCHAR(256) = 'arn:aws:s3:::<bucket_name>';
+DECLARE @s3_bucket_name NVARCHAR(256) = '<bucket_name>';
 SELECT 
 	CONCAT_WS(
 	CHAR(13) + CHAR(10),
 	'exec msdb.dbo.rds_restore_database',
 	CONCAT('@restore_db_name= ''', name, ''','),
-	CONCAT('@s3_arn_to_restore_from=''', @s3_bucket_arn,'/', name, '_db_backup.bak'';')
+	CONCAT('@s3_arn_to_restore_from=''arn:aws:s3:::', @s3_bucket_name,'/', name, '_db_backup.bak'';')
 	)
 FROM sys.databases
 WHERE database_id > 4 AND name NOT IN ('rdsadmin')
